415554c0ea4bbd3c8bff92e35554cecf9529ac2a
mfalm3 <waitha28ka@gmail.com>
2022-11-04 07:51:54 +0000

    Create homepage content
    Create homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,173 +1,121 @@
 import * as React from "react"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import axios from "axios"
+import { useState, useEffect } from "react"
+import Layout from "../components/layout"
+import CarouselSlider from "../components/slider"
+import MovieCard from "../components/movieCard"
+import GenreCard from "../components/genreCard"
 
 const IndexPage = () => {
+
+  const carouselOptions = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 3
+  }
+
+  const [popular, setPopular] = useState([])
+  const [trending, setTrending] = useState([])
+  const [genres, setGenres] = useState([])
+  const [error, setError] = useState('')
+
+  function getPopular() {
+    axios.get(`${process.env.GATSBY_TMDB_BASE_API_URL}/movie/popular?api_key=${process.env.TMDB_API}`)
+    .then((res) =>{
+      setPopular(res.data.results)
+      console.log('res', res.data)
+    }).catch(err => {
+      setError(error)
+    })
+  }
+
+  function getTrending() {
+    axios.get(`${process.env.GATSBY_TMDB_BASE_API_URL}/trending/movie/day?api_key=${process.env.TMDB_API}`)
+    .then((res) =>{
+      setTrending(res.data.results)
+    }).catch(err => {
+      setError(error)
+    })
+  }
+  function getGenres() {
+    axios.get(`${process.env.GATSBY_TMDB_BASE_API_URL}/genre/movie/list?api_key=${process.env.TMDB_API}`)
+    .then((res) =>{
+      console.log(res)
+      setGenres(res.data.genres)
+    }).catch(err => {
+      setError(error)
+    })
+  }
+
+  useEffect( () => {
+    getTrending()
+    getPopular()
+    getGenres()
+  },[])
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <Layout>
+      <main>
+        <div id="hero" className="p-4 relative 2xl:h-[15vw] m-auto">
+          <img src="" alt="" />
+          <div className="absolute inset-0 opacity-75 bg-slate-500 flex flex-col p-16 gap-4">
+            <div className="flex flex-col gap-4">
+              <h3 className="text-4xl font-bold text-white">Welcome.</h3>
+              <h5 className="text-3xl font-semibold text-white">Lots of fun films to binge. Look around</h5>
+            </div>
+            <div className="relative">
+              <input type="text" name="search" id="search" className="absolute rounded-3xl xl:w-full border p-3" placeholder="Search Movie"/>
+              <input type="submit" value="Search"  className="absolute right-0 rounded-3xl xl:w-[7vw] p-[0.80rem] bg-green-600 hover:cursor-pointer hover:text-white"/>
+            </div>
+          </div>
+        </div>
+
+        <div id="trending" className="my-12">
+          <h3 className="text-3xl font-bold">Trending films</h3>
+          {trending.length ? (
+            <CarouselSlider {...{carouselOptions}}>
+            { trending.map( movie => {
+              return <MovieCard movie={movie} key={movie.id}/>
+              }) 
+            }
+          </CarouselSlider>
+          ) : (
+            <div>No Movies Found</div>
+          )}
+        </div>
+
+        <div id="latest" className="my-12">
+          <h3 className="text-3xl font-bold">Popular films</h3>
+          {popular.length ? (
+            <CarouselSlider {...{carouselOptions}}>
+            { popular.map( movie => {
+              return <MovieCard movie={movie} key={movie.id}/>
+              }) 
+            }
+          </CarouselSlider>
+          ) : (
+            <div>No Movies Found</div>
+          )}
+        </div>
+
+        <div id="genres" className="my-12">
+          <h3 className="text-3xl font-bold">Genres</h3>
+          { genres.length ? (
+            <CarouselSlider {...{carouselOptions}}>
+            { genres.map( genre => {
+              return <GenreCard genre={genre} key={genre.id}/>
+              }) 
+            }
+          </CarouselSlider>
+          ) : (
+            <div>No Movies Found</div>
+          )}
+          
+        </div>
+      </main>
+    </Layout>
   )
 }
 
