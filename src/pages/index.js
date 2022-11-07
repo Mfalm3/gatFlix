2716e07bef71f1f9c0fa8dd8ff74209465b4def6
mfalm3 <waitha28ka@gmail.com>
2022-11-07 12:43:45 +0000

    Flesh out genre card to genre template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import axios from "axios"
 import { useState, useEffect } from "react"
+import { Link, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { isEmpty } from "lodash"
 import Layout from "../components/layout"
 import CarouselSlider from "../components/slider"
 import MovieCard from "../components/movieCard"
-import GenreCard from "../components/GenreCard"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
 
   const carouselOptions = {
     dots: true,
@@ -26,7 +28,7 @@ const IndexPage = () => {
     .then((res) =>{
       setPopular(res.data.results)
       console.log('res', res.data)
-    }).catch(err => {
+    }).catch(error => {
       setError(error)
     })
   }
@@ -40,13 +42,20 @@ const IndexPage = () => {
     })
   }
   function getGenres() {
-    axios.get(`${process.env.GATSBY_TMDB_BASE_API_URL}/genre/movie/list?api_key=${process.env.TMDB_API}`)
-    .then((res) =>{
-      console.log(res)
-      setGenres(res.data.genres)
-    }).catch(err => {
-      setError(error)
-    })
+    const genres = JSON.parse(localStorage.getItem('genres'))
+    console.log('genres', genres)
+    if( !isEmpty(genres) ){
+      setGenres(genres)
+    }else{  
+      axios.get(`${process.env.GATSBY_TMDB_BASE_API_URL}/genre/movie/list?api_key=${process.env.TMDB_API}`)
+      .then((res) =>{
+        console.log(res)
+        setGenres(res.data.genres)
+        localStorage.setItem('genres', JSON.stringify(res.data.genres))
+      }).catch(err => {
+        setError(error)
+      })
+    }
   }
 
   useEffect( () => {
@@ -54,6 +63,9 @@ const IndexPage = () => {
     getPopular()
     getGenres()
   },[])
+
+  // genre img nodes 
+  const nodes = data.allFile.nodes
 
   return (
     <Layout>
@@ -104,8 +116,24 @@ const IndexPage = () => {
           <h3 className="text-3xl font-bold">Genres</h3>
           { genres.length ? (
             <CarouselSlider {...{carouselOptions}}>
-            { genres.map( genre => {
-              return <GenreCard genre={genre} key={genre.id}/>
+            { genres.map( (genre) => {
+              const slug = genre.name.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-')
+              const image = nodes.find(img => img.childImageSharp.fluid.originalName.slice(0, -4) === slug)
+              return (
+                <div className='p-3' key={genre.id}>
+                  <div className='shadow-lg hover:shadow-2xl rounded-xl bg-white cursor-pointer'>
+                    <Link to={'/genre/'+ slug}>
+                      <div className="p-4">
+                        <GatsbyImage  
+                          image={image.childImageSharp.gatsbyImageData} 
+                          alt={genre.name}
+                        />
+                        <h3 className="text-base font-semibold min-h-[48px]">{ genre.name }</h3>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              )
               }) 
             }
           </CarouselSlider>
@@ -122,3 +150,24 @@ const IndexPage = () => {
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+
+export const Imgs = graphql `
+query ImgQuery {
+  allFile {
+    nodes {
+      childImageSharp {
+        gatsbyImageData(
+          placeholder: BLURRED
+          formats: [AUTO]
+          layout: CONSTRAINED
+          height: 600
+          width: 600
+        )
+        fluid {
+          originalName
+        }
+      }
+    }
+  }
+}
+`
