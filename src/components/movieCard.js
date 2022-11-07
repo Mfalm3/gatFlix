8c54dea56383706e87e88ee1394d1316143fd493
mfalm3 <waitha28ka@gmail.com>
2022-11-07 12:41:05 +0000

    Update single movie route
    Update single movie route

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -5,11 +5,10 @@ export default function MovieCard({movie}) {
 
   const slug = movie.title.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
 
-  console.log('movicard props',movie)
   return (
     <div className='p-3'>
       <div className='shadow-lg hover:shadow-2xl rounded-xl bg-white cursor-pointer'>
-        <Link to={slug}>
+        <Link to={'/movie/'+slug}>
           <div className='h-[300px] rounded-t-xl' style={ {backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.poster_path})`, backgroundPosition: `center top`}}>
             {/* <img className='w-full h-full' src={ `https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={ movie.title } /> */}
           </div>
